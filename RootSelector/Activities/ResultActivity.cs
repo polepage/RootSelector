8682d81cbc6eef2f3f9864075c66f2324e28b4d1
Pierre Olivier LePage <plepage@lti.ca>
2020-07-22 16:16:15 -0400

    Barebone Activities

---
+++ RootSelector/Activities/ResultActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace RootSelector.Activities
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar")]
+    public class ResultActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_results);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+        }
+    }
+}
---
+++ RootSelector/Activities/TableSetupActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Widget;
+
+namespace RootSelector.Activities
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class TableSetupActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_table);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            RegisterButtons();
+        }
+
+        private void RegisterButtons()
+        {
+            var toGame = FindViewById<Button>(Resource.Id.btn_to_game);
+            toGame.Click += NavigateToGame;
+        }
+
+        private void NavigateToGame(object sender, System.EventArgs e)
+        {
+            // Check conditions before advancing
+            var navigation = new Intent(this, typeof(GameSetupActivity));
+            StartActivity(navigation);
+        }
+    }
+}
